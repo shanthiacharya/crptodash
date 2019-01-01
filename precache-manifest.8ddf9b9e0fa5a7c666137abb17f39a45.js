0b0f68fd74349a98d558c1319c6200dc9d0668fe
shanthiacharya <{ID}+{username}@users.noreply.github.com>
2019-01-01 12:07:20 -0800

    Updates

---
+++ precache-manifest.8ddf9b9e0fa5a7c666137abb17f39a45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86209aef53401187456c",
+    "url": "/crptodash/static/js/runtime~main.86209aef.js"
+  },
+  {
+    "revision": "6a1b1f7966806916ab33",
+    "url": "/crptodash/static/js/main.6a1b1f79.chunk.js"
+  },
+  {
+    "revision": "ce9b73d17e5b50f42678",
+    "url": "/crptodash/static/js/1.ce9b73d1.chunk.js"
+  },
+  {
+    "revision": "6a1b1f7966806916ab33",
+    "url": "/crptodash/static/css/main.0bb7bcaa.chunk.css"
+  },
+  {
+    "revision": "4f0414bbee41c26e4268344cb3a222bd",
+    "url": "/crptodash/index.html"
+  }
+];
