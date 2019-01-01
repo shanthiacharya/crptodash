04591931888bc49b0cd247d41ec6d9cbeb122a21
shanthiacharya <{ID}+{username}@users.noreply.github.com>
2018-12-31 17:51:33 -0800

    Added LightTheme

--- src/Dashboard/HighchartsTheme.js
+++ src/Dashboard/HighchartsTheme.js
@@ -1,3 +1,5 @@
+import {lightTheme} from '../Shared/Styles';
+
 export default  {
    
         colors: [
@@ -11,11 +13,11 @@ export default  {
             '#1B2839'
         ],
         chart: {
-            backgroundColor: '#061a44',
+            backgroundColor: lightTheme ? 'white' : '#061a44',
             borderColor: '#000000',
             borderWidth:0,
             className: 'dark-container',
-            plotBackgroundColor: '#061a44',
+            plotBackgroundColor: lightTheme ? 'white':'#061a44',
            
             plotBorderWidth: 0
         },
